0ada1f085fa6192251582f7121efe3a9731318ed
wintstar <github@stephan-frank.de>
2024-01-15 13:37:31 +0100

    Delete all cookies
    fixed

--- public/js/chat.js
+++ public/js/chat.js
@@ -3002,15 +3002,13 @@ var ajaxChat = {
 	},
 
 	deleteCookies: function() {
-		if(!document.cookie)
+		if(!this.sessionName)
 			return null;
 
-		var allCookies = document.cookie.split(';');
-
-		// The "expire" attribute of every cookie is
-		// Set to "Thu, 01 Jan 1970 00:00:00 GMT"
-		for (var i = 0; i < allCookies.length; i++)
-			document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
+		this.createCookie(this.sessionName,'', -1);
+		this.createCookie(this.sessionName + '_lang','', -1);
+		this.createCookie(this.sessionName + '_settings', '', -1);
+		this.createCookie(this.sessionName + '_style','',-1);
 	},
 
 	isCookieEnabled: function() {
