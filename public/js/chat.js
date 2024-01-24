eea9385457fda975960b4139b0a26cef37498f18
zwiebus <zwiebus@users.noreply.github.com>
2024-01-24 15:16:53 +0100

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1367,7 +1367,7 @@ var ajaxChat = {
 				+ this.getChatListUserNameTitle(userID, userName, userRole, ip)
 				+ ' dir="'
 				+ this.baseDirection
-				+ '" onclick="ajaxChat.insertText(this.firstChild.nodeValue);">'
+				+ '" onclick="ajaxChat.insertText(\'[i]\'+this.firstChild.nodeValue+\'[/i][b] &#187; [/b]\');">'
 				+ userName
 				+ '</span>'
 				+ colon
